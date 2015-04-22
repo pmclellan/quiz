2961977185a64026989807a4cac78af949682788
pmclellan <paulmclellan_00@yahoo.com>
2015-04-22 14:37:54 -0700

    Refactored Parser Class
    - Applied 'final' keyword to relevant methods and variables
    - Assigned more descriptive names to variables
    - Added 'try... finally' blocks to ensure input and output streams are closed after use
    - Replaced synchronized get/set methods with ReadWriteLock
    - Introduced constant to represent Unicode threshold character
    - Resolved minor Checkstyle and PMD violations

--- Parser.java
+++ Parser.java
@@ -1,42 +1,78 @@
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
+    
+  private static final int UNICODE_THRESHOLD = 0x80;
+  private final ReadWriteLock fileLock = new ReentrantReadWriteLock();
   private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+    
+  public final void setFile(final File newFile) {
+    this.fileLock.writeLock().lock();
+    try {
+      this.file = newFile;
+    } finally {
+      this.fileLock.writeLock().unlock();
     }
-    return output;
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+    
+  public final File getFile() {
+    this.fileLock.readLock().lock();
+    try {
+      return this.file;
+    } finally {
+      this.fileLock.readLock().unlock();
+    }
+  }
+    
+  public final String getContent() throws IOException {
+    final FileInputStream input = new FileInputStream(this.getFile());
+    try {
+      final StringBuffer output = new StringBuffer();
+      int data = input.read();
+      while (data > 0) {
+        output.append((char) data);
+        data = input.read();
       }
+      return output.toString();
+    } finally {
+      input.close();
     }
-    return output;
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+    
+  public final String getContentWithoutUnicode() throws IOException {
+    final FileInputStream input = new FileInputStream(this.getFile());
+    try {
+      final StringBuffer output = new StringBuffer();
+      int data = input.read();
+      while (data > 0) {
+        if (data < UNICODE_THRESHOLD) {
+          output.append((char) data);
+        }
+        data = input.read();
+      }
+      return output.toString();
+    } finally {
+      input.close();
+    }
+  }
+    
+  public final void saveContent(final String content) throws IOException {
+    final FileOutputStream output = new FileOutputStream(this.getFile());
+    try {
+      for (int index = 0; index < content.length(); index += 1) {
+        output.write(content.charAt(index));
+      }
+    } finally {
+      output.close();
     }
   }
 }
